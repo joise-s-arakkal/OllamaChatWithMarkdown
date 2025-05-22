62c6f7cef61138be4e9f0e37ffab9a5e7bd0ef76
joise-s-arakkal <joisesarakkal007@gmail.com>
2025-05-22 19:54:30 +0530

    Improved the design

--- Flask-app.py
+++ Flask-app.py
@@ -34,4 +34,4 @@ def chat():
     return Response(stream_ollama_response(user_input), mimetype='text/event-stream')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5001,host="0.0.0.0")
